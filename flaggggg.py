dbe4dc739c07238327f7b7e1ab54ea7e2ad2755b
lazyprogrammers <74198408+sarbjeet890@users.noreply.github.com>
2021-03-22 19:48:12 +0530

    Update flaggggg.py

--- flaggggg.py
+++ flaggggg.py
@@ -7,10 +7,13 @@ font2 = "Midnight-minutes"
 
 #screen for output
 screen = turtle.Screen()
+screen.setup(width = 1364 , height = 720)
 # Defining a turtle Instance
 t = turtle.Turtle()
-t.speed(50)
-t.fillcolor("black")
+t.speed(1)
+
+
+turtle.shape("circle")
 # initially penup()
 t.penup()
 t.goto(-400, 250)
@@ -60,6 +63,7 @@ t.circle(60)
 t.end_fill()
  
 # Mini Blue Circles
+t.speed(11)
 t.penup()
 t.goto(-57, -8)
 t.pendown()
@@ -81,6 +85,7 @@ t.begin_fill()
 t.circle(20)
 t.end_fill()
 # Spokes
+t.speed(1)
 t.penup()
 t.goto(0, 0)
 t.pendown()
@@ -104,13 +109,41 @@ t.forward(800)
 t.right(90)
 t.forward(167*3)
 
-t.penup()
-t.goto(-300, -300)
-t.pendown()
-t.write("SUBSCRIBE TO MY YOUTUBE CHANNEL LAZY PROGRAMMERS", font=(font2,15, "normal"))
 
 t.penup()
-t.goto(-300, -320)
+t.goto(-400, -300)
+t.pendown()
+t.write("SUBSCRIBE TO MY YOUTUBE CHANNEL-       /LAZY PROGRAMMERS(link in the description)", font=(font2,15, "normal"))
+
+turtle.color("navy")
+t.penup()
+t.goto(-10,-290)
+t.pendown()
+t.color("red")
+t.hideturtle()
+t.setheading(90)
+t.begin_fill()
+t.fillcolor("red")
+for i in range(2):
+    t.forward(10)
+    t.circle(-10,90)
+    t.fd(15)
+    t.circle(-10,90)
+t.end_fill()
+t.up()
+t.goto(1,-289)
+t.down()
+t.color("white")
+t.begin_fill()
+t.fillcolor("white")
+for i in range(3):
+    t.fd(10)
+    t.right(120)
+t.end_fill()
+
+t.color("black")
+t.penup()
+t.goto(-280, -320)
 t.pendown()
 t.write("Follow me on instagram - @sarb.theiitain.6542", font=(font2,15, "normal"))
 
